82ea6d62fd3d0733262b01c74a5b07938393e4b8
Santiago Bandiera <sagaban@hotmail.com>
2020-01-11 16:50:49 -0300

    Add loop, attemprs counter and error handling

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,35 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    let mut attempts = 0;
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
 
-    println!("You guessed: {}", guess);
+        // println!("You guessed: {}", guess);
+        attempts += 1;
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win in {} attempts!", attempts);
+                break;
+            }
+        }
     }
 }
