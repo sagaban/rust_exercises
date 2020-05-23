997797890719d0a1271aa1f04dad48d8042b4f92
Santiago Bandiera <sagaban@hotmail.com>
2020-05-23 17:07:25 -0300

    Change app structure

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,7 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+mod exercises;
+
+use exercises::guessing;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    let mut attempts = 0;
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
-        };
-
-        // println!("You guessed: {}", guess);
-        attempts += 1;
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win in {} attempts!", attempts);
-                break;
-            }
-        }
-    }
+    guessing::guessing_game();
 }
